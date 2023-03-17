8b7ef16d3664532fd7154e365b17f780cc541ae8
Pipe Daza <pipedaza23@gmail.com>
2023-03-16 22:46:44 -0300

    introduccion

--- Portafolio/Portafolio/settings.py
+++ Portafolio/Portafolio/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'Portafolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [f"{BASE_DIR}/Portafolio/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,6 +127,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
